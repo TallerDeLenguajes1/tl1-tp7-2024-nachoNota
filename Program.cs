2c257244613dcc931a4cb6b2eb939008784de395
aleLobo17 <lobo.rodrigoalejandro.2020@gmail.com>
2024-05-21 15:23:01 -0300

    calculadora con clase hecha

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EspacioCalculadora;
+
+Calculadora miCalculadora = new Calculadora();
+
+int seguir = 0;
+
+do{
+    System.Console.WriteLine("1- Sumar");
+    System.Console.WriteLine("2- Restar");
+    System.Console.WriteLine("3- Multiplicar");
+    System.Console.WriteLine("4- Dividir");
+    System.Console.WriteLine("5- Limpiar");
+    System.Console.WriteLine("Opcion: ");
+    int opcion = int.Parse(Console.ReadLine());
+
+    System.Console.WriteLine("Ingrese numero: ");
+    int num = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1: miCalculadora.Sumar(num);
+            break;
+        case 2: miCalculadora.Restar(num);
+            break;
+        case 3: miCalculadora.Multiplicar(num);
+            break;
+        case 4: if(num != 0){
+                    miCalculadora.Dividir(num);
+                } else {
+                    System.Console.WriteLine("No se puede dividir en 0");
+                }
+            break;
+        case 5: miCalculadora.Limpiar();
+            break;
+        default: System.Console.WriteLine("Opcion no valida");
+            break;
+    }
+
+    System.Console.WriteLine("Realizar otra operacion? 1=si, 0=no");
+    seguir = int.Parse(Console.ReadLine());
+    
+}while(seguir == 1);
+
+System.Console.WriteLine($"Resultado: {miCalculadora.Resultado}");
--- obj/Debug/net8.0/tl1-tp7-2024-nachoNota.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl1-tp7-2024-nachoNota.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl1-tp7-2024-nachoNota")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60be059f2fe336738161e3b8001364c4a5d40546")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl1-tp7-2024-nachoNota")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl1-tp7-2024-nachoNota")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
